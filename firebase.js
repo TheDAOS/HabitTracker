e95f72055af27a6ffcbfd632eecf24d6570a5f31
theDAOS <sanjusanthosh46@gmail.com>
2025-04-04 23:26:23 +0530

    createChallenge

--- firebase.js
+++ firebase.js
@@ -716,8 +716,68 @@ window.createChallenge = async function (event) {
     const startDate = document.getElementById('start-date').value;
     const endDate = document.getElementById('end-date').value;
 
-    
+    // habit template
+    const habitName = document.getElementById('habit-name').value;
+    const habitDescription = document.getElementById('habit-description').value;
+    const habitCategory = document.getElementById('habit-category').value;
+    const habitType = document.getElementById('type').value;
+    const habitDifficulty = document.getElementById('difficulty').value;
+    const habitGoal = document.getElementById('goal').value;
+    const habitReminder = document.getElementById('reminders').checked;
+    const habitReminderTime = document.getElementById('reminder-time').value;
+    const checkboxes = document.querySelectorAll('.day');
 
+    const checkedDays = [];
+    const weekdays = [
+        "Sunday", "Monday",
+        "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday"
+    ];
+
+    checkboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+            checkedDays.push(weekdays.indexOf(checkbox.value));
+        }
+    });
+
+    const userRef = collection(db, "challenges");
+    const user = auth.currentUser.uid;
+    try {
+
+        await addDoc(userRef, {
+            createdBy: user,
+            participants: [user],
+            name: challengeName,
+            template: {
+                name: habitName,
+                description: habitDescription,
+                category: habitCategory,
+                type: habitType,
+                difficulty: habitDifficulty,
+                createdAt: new Date(),
+                lastUpdated: new Date(),
+                streak: 0,
+                totalCompleted: 0,
+                goal: habitGoal * 1,
+                log: 0,
+                history: [],
+                reminder: (habitReminder) ? {
+                    frequency: habitReminderTime,
+                    days: checkedDays,
+                } : false,
+            },
+            createdAt: new Date(startDate),
+            endAt: new Date(endDate),
+        })
+
+        alert("Challenge created successfully");
+        refreshData();
+        navigation.showChallenges();
+        
+    } catch (error) {
+        console.error("Error adding challenge: ", error);
+        alert("Error adding challenge");
+    }
 }
 
 window.showChallenges = async function () {
